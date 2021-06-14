2b0024a279603f5a9e6b17de09ac9f33ea5edd43
shunyasaitama <85655151+shunyasaitama@users.noreply.github.com>
2021-06-14 13:27:37 +0900

    resourcesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#show"
+  resources :tasks, only: [:index, :new, :edit, :show, :create, :destroy]
 end
