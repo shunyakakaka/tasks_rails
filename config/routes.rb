c89c63729ab0a0d7135b5f3fbfffdf4e674296c7
shunyasaitama <85655151+shunyasaitama@users.noreply.github.com>
2021-06-14 11:28:37 +0900

    ルーティングの直し

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new"
   post "tasks", to: "tasks#create"
-  get "tasks/show", to: "tasks#show"
+  get "tasks/:id", to: "tasks#show"
 end
